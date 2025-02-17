77d71281b62e2fc554287365b3e79e82f273b38c
MinSung Jung <minpro22@naver.com>
2025-02-17 17:33:37 +0900

    25.02.17 second commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,14 +2,17 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.") 기능이 약해서, 권장X
 public class Item {
 
+    @NotNull //수정 요구사항 추가 -> 수정시 id는 필수값
     private Long id;
 
     @NotBlank
@@ -20,7 +23,7 @@ public class Item {
     private Integer price;
 
     @NotNull
-    @Max(9999)
+//    @Max(9999) //수정 요구사항 추가 -> 수량은 무제한
     private Integer quantity;
 
     public Item() {
